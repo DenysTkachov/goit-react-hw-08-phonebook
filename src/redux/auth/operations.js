d9af6696519194233f981a67340c4b5dadfc4356
DenysTkachov <D.Tkachev@ukr.net>
2023-12-15 16:47:40 +0100

    fix

--- src/redux/auth/operations.js
+++ src/redux/auth/operations.js
@@ -13,7 +13,6 @@ const clearAuthHeader = () => {
   axios.defaults.headers.common.Authorization = '';
 };
 
-
 export const register = createAsyncThunk(
   'auth/register',
   async (credentials, thunkAPI) => {
